19f8ceb2b776c4c43bba15775652ba413fb526c4
phengemuhle <phengemuhle@gmail.com>
2018-10-23 12:53:55 -0600

    added json file and fetch

--- src/component/App.js
+++ src/component/App.js
@@ -6,32 +6,30 @@ import CartHeader from './CartHeader';
 import DropdownMenu from './Dropdown';
 import Quantity from './Quantity';
 import Submit from './Submit'
+// import products from './products.json'
 
 class App extends Component {
   constructor(props) {
-    super()
+    super(props)
     this.state = {
       ChosenItem: [],
       SelectedIds: [],
       year: "2016",
       quantity: 1,
-      total: 0
+      total: 0,
+      products: []
     }
     this.submit = this.submit.bind(this)
   }
-  const products = [
-    { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
-    { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
-    { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
-    { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
-    { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
-    { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
-    { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
-    { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
-    { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
-  ];
+  async componentDidMount() {
+    fetch("./products.json", {
+    }).then((result) => result.json())
+      .then((response) => this.setState({
+        products: response
+      }))
 
 
+  }
 
   updateQuantity = (event) => {
     this.setState({
@@ -46,17 +44,18 @@ class App extends Component {
     })
   }
 
+
   NewTotal = () => {
     let newtotal = 0
     this.state.ChosenItem.map(item => {
       newtotal += (((item.product.priceInCents) / 100) * this.state.quantity)
     })
-    return newtotal
+    return newtotal.toFixed(2)
   }
 
   submit() {
     var newList = this.state.SelectedIds.map(id => {
-      return products.filter(item => {
+      return this.state.products.filter(item => {
         return item.id === id
       })
     })
@@ -80,7 +79,7 @@ class App extends Component {
         <CartItems ChosenItem={this.state.ChosenItem} />
         <h3>Total: {this.NewTotal()}</h3>
         <Quantity updateQuantity={this.updateQuantity} value={this.state.quantity} />
-        <DropdownMenu updateSelected={this.updateSelected} products={products} value={this.state.SelectedId} />
+        <DropdownMenu updateSelected={this.updateSelected} products={this.state.products} value={this.state.SelectedId} />
         <Submit submit={this.submit} />
         <CartFooter year={this.state.year} />
       </div>
